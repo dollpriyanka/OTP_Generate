572202fe492ef161aecba4fc24d5e6a281702049
priyanka chatterjee <chatterjeepriyanka988@gmail.com>
2022-04-02 11:17:38 +0530

    OTP

---
+++ otp.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class otp {
+	static char[] OTP(int len) {
+		System.out.println("Generating OTP using random():");
+		System.out.print("Your OTP is : ");
+		String numbers = "0123456789";
+		Random rndm_method = new Random();
+		char[] otp = new char[len];
+		for(int i = 0;i < len; i++) {
+			otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+		}
+		return otp;
+	}
+	public static void main(String[] args) {
+		int length = 4;
+		System.out.println(OTP(length));
+	}
+}
